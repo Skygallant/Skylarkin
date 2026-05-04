41f0543f07e7407a651301b56627cfce0ca59114
Sarren Skygallant <sarren@skygallant.com>
2026-05-04 10:48:19 +0100

    Minor strings.xml tweak.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.skylarkin.evfinder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "0.19"
+        versionCode = 20
+        versionName = "0.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
