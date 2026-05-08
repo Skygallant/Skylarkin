f21539f73256be29239ba6dc5ac1bbee18efda31
Sarren Skygallant <sarren@skygallant.com>
2026-05-08 20:15:09 +0100

    fix for minsdk 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.skylarkin.evfinder"
         minSdk = 26
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.25"
+        versionCode = 26
+        versionName = "0.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
